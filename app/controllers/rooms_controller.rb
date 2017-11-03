2fb4254daedeab0228871155ba73ca41aaf93302
Kmilo1488 <camilomoreno1488@live.com>
2017-11-03 00:00:19 -0500

    final version

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,14 +16,23 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room =  Room.find(params[:id])
+    @room.update(room_params)
+    redirect_to rooms_path
+  end
   def destroy
-    romm = Room.find(params[:id])
-    room.destroy
+    @room = Room.find(params[:id])
+    @room.destroy
     redirect_to rooms_path
   end
 
   protected
-    def room_params
-      params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
-    end
+  def room_params
+    params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
+  end
 end
