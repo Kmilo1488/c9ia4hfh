c39a60bdd7f6c04c5e84eef68178df4b053ccd75
Kmilo1488 <camilomoreno1488@live.com>
2017-11-02 22:38:11 -0500

    add edit room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,18 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(rooms_path)
+    else
+      render :edit
+    end
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
